c25b0b19850a8d3c4a3a485d93500cbcd0b0b9d3
Bresiu <michal.brewczak@gmail.com>
2017-02-21 12:45:05 +0100

    made cube, face template objects, furute move parse

--- O-RubiksCube/main.cpp
+++ O-RubiksCube/main.cpp
@@ -10,16 +10,80 @@
 #include <vector>
 #include <regex>
 #include <assert.h>
+#include <algorithm>
+#include <map>
+
 using namespace std;
 
-vector<string> parseMovesChain(string faceMovesChain) {
+class Face {
+private:
+    int faceType;
+    int fields[9];
+public:
+    Face(int faceType) {
+        Face::faceType = faceType;
+        fill_n(fields, 9, faceType);
+    }
+};
+
+class Cube {
+    map<char, Face> faces = {
+        pair<char, Face> ('F', Face(6)),
+        pair<char, Face> ('B', Face(5)),
+        pair<char, Face> ('R', Face(4)),
+        pair<char, Face> ('L', Face(3)),
+        pair<char, Face> ('U', Face(2)),
+        pair<char, Face> ('D', Face(1))
+    };
+public: void doMove(char move, int moveValue) {
+    switch (move) {
+        case 'F':
+            
+            break;
+        case 'B':
+            
+            break;
+        case 'R':
+            
+            break;
+        case 'L':
+            
+            break;
+        case 'U':
+            
+            break;
+        case 'D':
+            
+            break;
+        default:
+            break;
+    }
+        cout << move << " by: " << moveValue << endl;
+    }
+    
+public: string toString() {
+        return "";
+    }
+};
+
+pair<char, int> parseMoveValue(string move) {
+    if (move.length() == 1) {
+        return make_pair(move[0], 1);
+    } else if (move[1] == '2') {
+        return make_pair(move[0], 2);
+    } else {
+        return make_pair(move[0], 3);
+    }
+}
+
+vector<pair<char, int>> parseMovesChain(string faceMovesChain) {
     regex regex("[B-U](?:2?'?)?", regex_constants::icase);
     smatch matcher;
-    vector<string> moves;
+    vector<pair<char, int>> moves;
     regex_iterator<string::iterator> iterator(faceMovesChain.begin(), faceMovesChain.end(), regex);
     regex_iterator<string::iterator> end;
     while (iterator != end) {
-        moves.push_back(iterator -> str());
+        moves.push_back(parseMoveValue(iterator -> str()));
         ++iterator;
     }
     return moves;
@@ -31,12 +95,17 @@ void print(const vector<string>& vector){
     }
 }
 
+void doMove(char move, int moveValue, Cube cube) {
+    cube.doMove(move, moveValue);
+}
+
 string computeFaceColors(string faceMovesChain) {
-    vector<string> moves = parseMovesChain(faceMovesChain);
-    for (string move : moves) {
-        doMove(move);
+    vector<pair<char, int>> moves = parseMovesChain(faceMovesChain);
+    Cube cube;
+    for (pair<char, int> move : moves) {
+        doMove(move.first, move.second, cube);
     }
-    return "";
+    return cube.toString();
 }
 
 int main(int argc, const char * argv[]) {
